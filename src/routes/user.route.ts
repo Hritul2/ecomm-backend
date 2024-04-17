5ba74ed8e11bfa233c91c2806a888faeda614d70
Hritul <hritul.srivastava@gmail.com>
2024-04-17 11:51:05 +0530

    define user routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,14 @@
-// user.rotue.ts
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+} from "../controllers/user.controller";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
 
 export default router;
